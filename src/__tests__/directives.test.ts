fa7a862581e97ee75d61ffbf1814a6b8538b8a42
Tong Jiang <tong.jiang@gmail.com>
2020-03-01 16:20:48 -0800

    Skeleton code that travels the schema

--- src/__tests__/directives.test.ts
+++ src/__tests__/directives.test.ts
@@ -1,5 +1,24 @@
-import { Greeter } from "../index";
+import gql from 'graphql-tag';
+import { StoreType, generateXdbSchema } from '../index';
 
-test("My greeter", () => {
-    expect(Greeter('Tong')).toBe('Hello Tong');
+const deviceSchema = gql`
+  type Device implements Entity @xdbEntity{
+    os: String
+    family: String
+    physicalInterfaces: [PhysicalInterface] @xdbChildren
+  }
+
+  type PhysicalInterface implements Entity @xdbEntity {
+    mac: String
+  }
+`;
+
+const dbSchema = generateXdbSchema({
+  schemas: [deviceSchema],
+  type: StoreType.MYSQL,
+  dbName: 'test',
+});
+
+test('Test db schema generation', () => {
+  expect(dbSchema).toBe('test');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,83 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+import gql from 'graphql-tag';
+import { makeExecutableSchema, SchemaDirectiveVisitor, ITypedef } from 'graphql-tools';
+import { GraphQLSchema, GraphQLObjectType, GraphQLField, GraphQLDirective, DirectiveLocation } from 'graphql';
+
+export const xdbSchema = gql`
+  directive @xdbEntity on OBJECT
+  directive @xdbParent on FIELD_DEFINITION
+  directive @xdbChildren on FIELD_DEFINITION
+  directive @xdbRefOneToMany on FIELD_DEFINITION
+  directive @xdbRefManyToOne on FIELD_DEFINITION
+  directive @xdbRefManyToMany on FIELD_DEFINITION
+
+  interface Entity {
+      uuid: String!
+      version: Int!
+      startSID: Int!
+      endSID: Int!
+      name: String!
+  }
+
+  type Query {
+      getEntityById(
+          type: String, 
+          uuid: String, 
+          dbSnapshot: Int = NULL
+      ): Entity
+  }
+`;
+
+interface IGetSchemaDirectivesOutput {
+  [directiveName: string]: typeof SchemaDirectiveVisitor;
+}
+
+export function getSchemaDirectives(): IGetSchemaDirectivesOutput {
+  class XdbDirective extends SchemaDirectiveVisitor {
+    public static getDirectiveDeclaration(name: string, schema: GraphQLSchema): GraphQLDirective {
+      const location = name === 'xdbEntity' ? DirectiveLocation.OBJECT : DirectiveLocation.FIELD_DEFINITION;
+      return new GraphQLDirective({
+        name,
+        locations: [location],
+      });
+    }
+    public visitObject(object: GraphQLObjectType) {
+      const msgType = 'debug';
+      console[msgType]('Visited object: ' + object.name);
+    }
+    public visitFieldDefinition(field: GraphQLField<any, any>, details: any) {
+      const msgType = 'debug';
+      console[msgType]('Visited field: ' + field.name);
+    }
+  }
+  return {
+    xdbEntity: XdbDirective,
+    xdbParent: XdbDirective,
+    xdbChildren: XdbDirective,
+    xdbRefOneToMany: XdbDirective,
+    xdbRefManyToOne: XdbDirective,
+    xdbRefManyToMany: XdbDirective,
+  };
+};
+
+export enum StoreType {
+  MYSQL = 'mysql',
+  PGSQL = 'postgres',
+  SQLITE = 'sqlite'
+};
+
+export interface IGenerateXdbSchemaInput {
+  schemas: ITypedef[],
+  type: StoreType,
+  dbName: string
+};
+
+export function generateXdbSchema(options: IGenerateXdbSchemaInput): string {
+  makeExecutableSchema({
+    typeDefs: options.schemas.concat(xdbSchema),
+    schemaDirectives: getSchemaDirectives(),
+    resolverValidationOptions: {
+      requireResolversForResolveType: false
+    }
+  });
+  return options.dbName;
+}
